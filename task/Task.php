4fe2dacf6c28a68f5dc8a5f6a929b6fa2716b0e9
snegodenko <snegodenko@gmail.com>
2022-04-15 19:38:08 +0300

    Обновил файл Task.php

--- task/Task.php
+++ task/Task.php
@@ -3,44 +3,58 @@
 
 class Task 
 {
-    private $action; 
-    private $actions; 
+    private $action = ''; // текущее действие
+    
 
-    public $customer; // id заказчика
-    public $executor; // id исполнителя
+    public $currentStatus = 'Не определен';
+    public $customer_id; // id заказчика
+    public $executor_id; // id исполнителя
+
+    public const STATUS_NEW = 'Подано новое объявление';
+    public const STASTUS_WORK = 'В работе';
+    public const STATUS_DONE = 'Выполнено';
+    public const STATUS_REFUSE = 'Провалено';
+
+    public const ACTION_CANCEL = 'Отменить';
+    public const ACTION_ANSWER = 'Откликнуться';
+    public const ACTION_DONE = 'Выполнено';
+    public const ACTION_REFUSE = 'Отказаться';
+
 
     private $map = [
        'new' => [
-           'status' => 'Подано новое объявление',
+           'status' => self::STATUS_NEW,
            'actions' => [
-            'cancel' => 'Отменить',
-            'answer' => 'Откликнуться']
+            'cancel' =>self::ACTION_CANCEL,
+            'answer' => self::ACTION_ANSWER]
            ],
         'work' => [
-            'status' => 'В работе',
+            'status' => self::STASTUS_WORK,
             'actions' => [
-                'done' => 'Выполнено',
-                'refuse' => 'Отказаться'
+                'done' => self::ACTION_DONE,
+                'refuse' => self::ACTION_REFUSE
                     ]
             ],
         'done_action' => [
-            'status' => 'Выполнено'
+            'status' => self::STATUS_DONE
         ],
         'refuse_action' => [
-            'status' => 'Провалено'
+            'status' => self::STATUS_REFUSE
         ]
 
-        ];
+    ];
     
     
 
-    public function __construct($action, $customer, $executor = 0)
+    public function __construct($action, $customer_id, $executor_id = 0)
     {
             $this->action = $action;
 
-            $this->customer = $customer;
+            $this->customer_id = $customer_id;
 
-            $this->executor = $executor;
+            $this->executor_id = $executor_id;
+
+            $this->getStatus();
 
     }
 
@@ -49,16 +63,12 @@ class Task
      */
     public function getActions()
     {
-        foreach($this->map as $key => $item){
+        if(array_key_exists($this->action, $this->map)){
 
-            if($this->action == $key){
-
-               $this->actions = $item['actions']; 
-
-            }
+           return $actions = $this->map[$this->action]['actions'];
         }
 
-        return $this->actions;
+        return false;
     }
 
 
@@ -68,17 +78,12 @@ class Task
      */
     public function getStatus()
     {   
-        $status = 'Не определен';
 
-       foreach($this->map as $key => $item){
+       if(array_key_exists($this->action, $this->map)){
 
-        if($this->action == $key){
-
-            $status = $item['status'];
-        } 
-
+        $this->currentStatus = $this->map[$this->action]['status'];
        }
 
-       return $status;
+       return $this->currentStatus;
     }
 }
